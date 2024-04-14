195471022bd650c63731edb97b09100361ac68d0
Siddharth Sahani <sidhucroedde@gmail.com>
2024-04-14 17:09:05 +0530

    changed form in checkout

--- checkout.php
+++ checkout.php
@@ -118,33 +118,12 @@ if(isset($_POST['order_btn'])){
             <select name="method">
                <option value="cash on delivery">cash on delivery</option>
                <option value="credit card">credit card</option>
-               <option value="paypal">paypal</option>
-               <option value="paytm">paytm</option>
+               <option value="upi">upi</option>
             </select>
          </div>
          <div class="inputBox">
             <span>address line 01 :</span>
             <input type="number" min="0" name="flat" required placeholder="e.g. flat no.">
-         </div>
-         <div class="inputBox">
-            <span>address line 01 :</span>
-            <input type="text" name="street" required placeholder="e.g. street name">
-         </div>
-         <div class="inputBox">
-            <span>city :</span>
-            <input type="text" name="city" required placeholder="e.g. mumbai">
-         </div>
-         <div class="inputBox">
-            <span>state :</span>
-            <input type="text" name="state" required placeholder="e.g. maharashtra">
-         </div>
-         <div class="inputBox">
-            <span>country :</span>
-            <input type="text" name="country" required placeholder="e.g. india">
-         </div>
-         <div class="inputBox">
-            <span>pin code :</span>
-            <input type="number" min="0" name="pin_code" required placeholder="e.g. 123456">
          </div>
       </div>
       <input type="submit" value="order now" class="btn" name="order_btn">
